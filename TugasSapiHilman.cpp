3533f633fb37307b4e053aee79536eadf154b402
hilmanhamidi <hilmanhamidi007@gmail.com>
2018-01-20 08:22:10 +0800

    bunyinya/suaranya

--- TugasSapiHilman.cpp
+++ TugasSapiHilman.cpp
@@ -2,6 +2,25 @@
 #include <string>
 using namespace std;
 
+class sapi{
+	public:
+		void bersuara(string bunyi);
+		int jumlahKumis;
+		string warnaBulu;
+};
+
 int main(){
+	sapi sapiDogol,sapibali;
 	
+	sapiDogol.warnaBulu = "PUTIH";
+	sapibali.warnaBulu = "COKLAT";
+	
+	cout << (sapiDogol.warnaBulu);
+	sapiDogol.bersuara("MOOOOOO");
+	
+	sapibali.bersuara("EMMOOOO");
+}
+
+void sapi::bersuara(string bunyi){
+	cout << "Bunyinya "<<bunyi<< " !!"<<endl;
 }
